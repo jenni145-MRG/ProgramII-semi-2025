6f8146c10982c02c18fd9a888bbb603f57fa5219
maricela renderos <maricelarenderos145@gmail.com>
2026-01-29 10:19:57 -0600

    primer hola mundo creacion de interfaz de una calculadora

--- miPrimeraApp/app/build.gradle.kts
+++ miPrimeraApp/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ugb.miprimeraapp"
-        minSdk = 22
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
