01171a478759e002b3dc44d76aa50a06a137a814
maricela renderos <maricelarenderos145@gmail.com>
2026-02-05 10:12:58 -0600

    primer hola mundo creacion de interfaz de una calculadora agregamos las opciones de suma,resta,multiplicacion y division

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ugb.miprimeraapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,57 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    Button btn;
+    RadioButton opt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v -> calcular());
+
     }
-}
+
+    private void calcular() {
+        tempVal = findViewById(R.id.txtNum1);
+        Double Num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.txtNum2);
+        Double Num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = 0;
+
+        opt=findViewById(R.id.optSuma);
+        if(opt.isChecked()){
+            respuesta= Num1 + Num2;
+        }
+
+        opt=findViewById(R.id.optResta);
+        if(opt.isChecked()){
+            respuesta= Num1 - Num2;
+        }
+
+        opt=findViewById(R.id.optMultiplicar);
+        if(opt.isChecked()){
+            respuesta= Num1 * Num2;
+        }
+
+        opt=findViewById(R.id.optDivision);
+        if(opt.isChecked()){
+            respuesta= Num1 / Num2;
+        }
+
+
+        opt=findViewById(R.id.optFactorial);
+        if(opt.isChecked()){
+            respuesta=;
+        }
+        tempVal = findViewById(R.id.txtRespuesta);
+        tempVal.setText("Respuesta:" + respuesta);
+    }
+};
+
