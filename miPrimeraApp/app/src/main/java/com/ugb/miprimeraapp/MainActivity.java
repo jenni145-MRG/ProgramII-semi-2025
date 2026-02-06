456bac92d8e4316e0694c3640b879d00c0f9d764
maricela renderos <maricelarenderos145@gmail.com>
2026-02-06 09:51:49 -0600

    primer hola mundo creacion de interfaz de una calculadora agregamos las opciones de suma,resta,multiplicacion, division, exponente y porcentaje Creacion de la rama conversores

--- miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
+++ miPrimeraApp/app/src/main/java/com/ugb/miprimeraapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,67 +14,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tempVal;
-    Button btn;
-    RadioButton opt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v -> calcular());
 
     }
 
-    private void calcular() {
-        tempVal = findViewById(R.id.txtNum1);
-        Double Num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.txtNum2);
-        Double Num2 = Double.parseDouble(tempVal.getText().toString());
-
-        double respuesta = 0;
-
-        opt=findViewById(R.id.optSuma);
-        if(opt.isChecked()){
-            respuesta= Num1 + Num2;
-        }
-
-        opt=findViewById(R.id.optResta);
-        if(opt.isChecked()){
-            respuesta= Num1 - Num2;
-        }
-
-        opt=findViewById(R.id.optMultiplicar);
-        if(opt.isChecked()){
-            respuesta= Num1 * Num2;
-        }
-
-        opt=findViewById(R.id.optDivision);
-        if(opt.isChecked()){
-            respuesta= Num1 / Num2;
-        }
 
 
-        opt=findViewById(R.id.optPorcentaje);
-        if(opt.isChecked()){
-            respuesta= Num1*Num2/100;
-        }
 
-        opt=findViewById(R.id.optExponente);
-        if(opt.isChecked()){
-            respuesta= Math.pow(Num1,Num2);//Math.pow(base,exponente)
-        }
 
-        opt=findViewById(R.id.optRaiz);
-        if(opt.isChecked()){
-            respuesta= Math.sqrt(Num1);
-        }
-        tempVal = findViewById(R.id.txtRespuesta);
-        tempVal.setText("Respuesta:" + Math.round(respuesta));//Math.round para redondear
+
     }
 };
 
