a07f513005c66491fecb831bf360d59ff9974494
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2026-03-13 15:21:00 -0600

    sqlite

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,105 +1,119 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tbh;
-    Spinner spnAreaDe, spnAreaA;
-    Button btn, btnAgua;
-    EditText txtMetros, txAreaCantidad;
-    TextView lblAguaRespuesta, lblAreaRespuesta;
-
-    Double valorArea[] = new Double[]{0.092903, 0.698756, 0.836127, 1.0, 436.722, 6987.557, 10000.0};
+    DB db;
+    Button btn;
+    TextView tempVal;
+    String accion="nuevo", idAmigo="", urlFoto;
+    Intent tomarFotoIntent;
+    FloatingActionButton fab;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        img = findViewById(R.id.imgFotoAmigo);
+        img.setOnClickListener(v->tomarFoto());
 
-        tbh = findViewById(R.id.tbhConversores);
-        tbh.setup();
+        db = new DB(this);
 
+        btn = findViewById(R.id.btnGuardarAmigo);
+        btn.setOnClickListener(v->guardarAmigo());
 
-        tbh.addTab(tbh.newTabSpec("Area")
-                .setContent(R.id.Area)
-                .setIndicator("Área", null));
-
-        tbh.addTab(tbh.newTabSpec("Agua")
-                .setContent(R.id.Agua)
-                .setIndicator("Agua", null));
+        fab = findViewById(R.id.fabListaAmigo);
 
 
-        spnAreaDe      = findViewById(R.id.AreaDe);
-        spnAreaA       = findViewById(R.id.spnAreaA);
-        txAreaCantidad = findViewById(R.id.txAreaCantidad);
-        lblAreaRespuesta = findViewById(R.id.lblAreaRespuesta);
+    }
+    private void tomarFoto(){
+        tomarFotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File fotoAmigo = null;
 
-        btn = findViewById(R.id.btnAreaConvertir);
-        btn.setOnClickListener(v -> convertirArea());
-
-
-        txtMetros        = findViewById(R.id.txtMetros);
-        lblAguaRespuesta = findViewById(R.id.lblAguaRespuesta);
-
-        btnAgua = findViewById(R.id.btnAguaCalcular);
-        btnAgua.setOnClickListener(v -> calcularAgua());
+        try{
+            fotoAmigo = crearImgAmigo();
+            if(fotoAmigo!=null){
+                Uri uriFoto = FileProvider.getUriForFile(MainActivity.this, "com.ugb.miprimeraapp.fileprovider", fotoAmigo);
+                tomarFotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriFoto);
+                startActivityForResult(tomarFotoIntent, 1);
+            }else{
+                mostrarMsg("Nose pudo crear la foto");
+            }
+        } catch (Exception e) {
+            mostrarMsg("Error al tomar la foto: "+ e.getMessage());
+        }
     }
 
-
-    private void convertirArea() {
-        String input = txAreaCantidad.getText().toString();
-
-        if (input.isEmpty()) {
-            lblAreaRespuesta.setText("Ingrese una cantidad");
-            return;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(requestCode==1 && resultCode==RESULT_OK){
+                img.setImageURI(Uri.parse(urlFoto));
+            }else{
+                mostrarMsg("No fue posible mostrar la foto");
+            }
+        } catch (Exception e) {
+            mostrarMsg("Error en abrir la camara: "+ e.getMessage());
         }
-
-        int de = spnAreaDe.getSelectedItemPosition();
-        int a  = spnAreaA.getSelectedItemPosition();
-        double cantidad  = Double.parseDouble(input);
-        double respuesta = conversor(de, a, cantidad, valorArea);
-
-        lblAreaRespuesta.setText(String.format("Respuesta: %.6f", respuesta));
     }
 
-
-    private void calcularAgua() {
-        String input = txtMetros.getText().toString();
-
-        if (input.isEmpty()) {
-            lblAguaRespuesta.setText("Ingrese los metros consumidos");
-            return;
+    private File crearImgAmigo() throws Exception{
+        String fechaHoraMs = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()),
+                fileMane = "foto_"+ fechaHoraMs;
+        File dirAlmacenamiento = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        if(dirAlmacenamiento.exists()==false){
+            dirAlmacenamiento.mkdir();
         }
-
-        double metros = Double.parseDouble(input);
-        double total;
-
-        if (metros <= 18) {
-
-            total = 6.0;
-
-        } else if (metros <= 28) {
-            total = 6.0 + (metros - 18) * 0.45;
-
-        } else {
-
-            total = 6.0 + (10 * 0.45) + (metros - 28) * 0.65;
-        }
-
-        lblAguaRespuesta.setText(String.format("Valor a pagar: $%.2f", total));
+        File image = File.createTempFile(fileMane, ".jpg", dirAlmacenamiento);
+        urlFoto = image.getAbsolutePath();
+        return image;
     }
+    private void guardarAmigo(){
+        tempVal = findViewById(R.id.txtNombreAmigos);
+        String nombre = tempVal.getText().toString();
 
+        tempVal = findViewById(R.id.txtDireccionAmigos);
+        String direccion = tempVal.getText().toString();
 
-    double conversor(int de, int a, double cantidad, Double[] valoresArray) {
-        return cantidad * valoresArray[de] / valoresArray[a];
+        tempVal = findViewById(R.id.txtTelefonoAmigos);
+        String tel = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtEmailAmigos);
+        String email = tempVal.getText().toString();
+
+        tempVal = findViewById(R.id.txtDuiAmigos);
+        String dui = tempVal.getText().toString();
+
+        String[] datos = {idAmigo, nombre, direccion, tel, email, dui, urlFoto};
+        db.administrar_amigos(accion, datos);
+        mostrarMsg("Registro de amigo guardado con exito.");
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
 }
