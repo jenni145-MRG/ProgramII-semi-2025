40d27f82c1c3d7d5367faaf142a143a5df9134cd
maricela renderos <maricelarenderos145@gmail.com>
2026-03-15 07:08:51 -0600

    tarea de programacion Creacion de la pestaña registro y productos

--- app/src/main/java/com/ugb/myapplication/MainActivity.java
+++ app/src/main/java/com/ugb/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TabHost;
 import android.widget.Toast;
@@ -17,71 +18,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etCodigo, etDescripcion, etMarca, etPrecio;
-
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
 
 
-        // Configuración de los bordes de la pantalla (EdgeToEdge)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    @SuppressLint("MissingSuperCall")
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+            TabHost host = findViewById(R.id.miTabHost);
+            host.setup();
 
+            TabHost.TabSpec spec1 = host.newTabSpec("tab1");
+            spec1.setIndicator("REGISTRO");
+            spec1.setContent(R.id.pestaña_registro);
+            host.addTab(spec1);
 
-        TabHost abh = findViewById(R.id.tbhTienda);
-        abh.setup();
+            TabHost.TabSpec spec2 = host.newTabSpec("tab2");
+            spec2.setIndicator("PRODUCTOS");
+            spec2.setContent(R.id.pestaña_producto);
+            host.addTab(spec2);
 
-        // 2. Configurar la pestaña de "Información"
-        TabHost.TabSpec spec = abh.newTabSpec("info");
-        spec.setContent(R.id.pestañaInfo); // Asegúrate de que en el XML sea idéntico (con o sin ñ)
-        spec.setIndicator("INFORMACIÓN");
-        abh.addTab(spec);
-
-        // 3. Crear pestañas para "Producto" y "Compra"
-        // Usamos el ID del FrameLayout o un TextView temporal para que no de error
-        abh.addTab(abh.newTabSpec("producto").setContent(R.id.textView).setIndicator("PRODUCTO"));
-        abh.addTab(abh.newTabSpec("compra").setContent(R.id.textView).setIndicator("COMPRA"));
-
-        etCodigo = findViewById(R.id.etCodigo);
-        etDescripcion = findViewById(R.id.etDescripcion);
-        etMarca = findViewById(R.id.etMarca);
-        etPrecio = findViewById(R.id.etPrecio);
+        TabHost.TabSpec spec3 = host.newTabSpec("tab3");
+        spec3.setIndicator("COMPRAS");
+        spec3.setContent(R.id.pestaña_compra);
+        host.addTab(spec3);
+        }
     }
-
-
-    public void registrar(View v) {
-        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "tienda", null, 1);
-        SQLiteDatabase bd = admin.getWritableDatabase();
-
-        ContentValues registro = new ContentValues();
-        registro.put("Codigo", etCodigo.getText().toString());
-        registro.put("Descripcion", etDescripcion.getText().toString());
-        registro.put("Marca", etMarca.getText().toString());
-        registro.put("Precio", etPrecio.getText().toString());
-
-        bd.insert("Informacion", null, registro);
-        bd.close();
-
-        etCodigo.setText("");
-        etDescripcion.setText("");
-        etMarca.setText("");
-        etPrecio.setText("");
-
-        Toast.makeText(this, "Producto guardado", Toast.LENGTH_SHORT).show();
-    }
-}
-
-
 
 
 
